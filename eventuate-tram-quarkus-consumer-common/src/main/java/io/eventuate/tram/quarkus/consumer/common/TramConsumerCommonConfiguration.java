2d7577eeef9a9a65403137656cb702654d84bcc9
Chris Richardson <chris@chrisrichardson.net>
2024-09-22 21:06:11 +0900

    Implemented SubscriberMapping/DefaultSubscriberMapping

--- eventuate-tram-quarkus-consumer-common/src/main/java/io/eventuate/tram/quarkus/consumer/common/TramConsumerCommonConfiguration.java
+++ eventuate-tram-quarkus-consumer-common/src/main/java/io/eventuate/tram/quarkus/consumer/common/TramConsumerCommonConfiguration.java
@@ -4,7 +4,10 @@ import io.eventuate.tram.consumer.common.DecoratedMessageHandlerFactory;
 import io.eventuate.tram.consumer.common.MessageConsumerImpl;
 import io.eventuate.tram.consumer.common.MessageConsumerImplementation;
 import io.eventuate.tram.messaging.common.ChannelMapping;
+import io.eventuate.tram.messaging.consumer.DefaultSubscriberMapping;
 import io.eventuate.tram.messaging.consumer.MessageConsumer;
+import io.eventuate.tram.messaging.consumer.SubscriberMapping;
+import io.quarkus.arc.DefaultBean;
 
 import javax.enterprise.inject.Instance;
 import javax.inject.Singleton;
@@ -15,7 +18,14 @@ public class TramConsumerCommonConfiguration {
   @Singleton
   public MessageConsumer messageConsumer(Instance<MessageConsumerImplementation> messageConsumerImplementation,
                                          Instance<ChannelMapping> channelMapping,
-                                         DecoratedMessageHandlerFactory decoratedMessageHandlerFactory) {
-    return new MessageConsumerImpl(channelMapping.get(), messageConsumerImplementation.get(), decoratedMessageHandlerFactory);
+                                         DecoratedMessageHandlerFactory decoratedMessageHandlerFactory, SubscriberMapping subscriberMapping) {
+    return new MessageConsumerImpl(channelMapping.get(), messageConsumerImplementation.get(), decoratedMessageHandlerFactory, subscriberMapping);
   }
+
+  @Singleton
+  @DefaultBean
+  public SubscriberMapping subscriberMapping() {
+    return new DefaultSubscriberMapping();
+  }
+
 }
